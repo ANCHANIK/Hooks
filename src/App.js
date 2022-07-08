f7738a9a5f3964948788a913274043f9b80cd8e3
cksdlr7446 <82303701+ANCHANIK@users.noreply.github.com>
2022-07-08 16:09:05 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./styles.css";
+
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+
+    setValue(value);
+  };
+
+  return { value, onChange };
+};
+
+export default function App() {
+  const name = useInput("ANCHANIK");
+
+  console.log(name);
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
+    </div>
+  );
+}
